13bcadaac044f1c0661a963471bd22a0790f8b72
Natalia da Silva <ndasilva@iastate.edu>
2018-07-03 15:57:36 +1000

    example with selected plants

--- code.R
+++ code.R
@@ -2,59 +2,106 @@
 library(tidyverse)
 library(ALA4R)
 library(vegan)
+library(RColorBrewer)
+
+##
+# Triodia, Brachychiton, Flindersia, Livistona, Callitris, Daviesia, Ficus, Hakea. Need more?
+#   
+#   Some look to be limited by rainfall (in wetter areas) but they are not in the north. Why not? What other factors might be limiting distribution?
+#   Some are limited to north and e coast? Why? Some of these are also in what looks like arid zone. Do some species live in very different conditions from other members of genus or are there small patches of matched climate.
+# Rainfall is important in biodiversity. Is there a gradient in biodiversity in line with rainfall gradients?
 
 
 
-#example from https://atlasoflivingaustralia.github.io/ALA4R/articles/ALA4R.html
 
-wkt <- "POLYGON((152.5 -35,152.5 -32,140 -32,140 -35,152.5 -35))"
+###NDS Examples
+brachychiton <- specieslist("brachychiton")  
+dim(brachychiton)
+#Not useful only counts
 
-## define some environmental layers of interest [see ala_fields()]
-env_layers <- c("Precipitation - annual","Temperature - annual max mean")
 
-## Download the data.  We use the `occurrences()` function, adding environmental
-##   data via the 'extra' parameter. Note that method="offline" supports
-##   unlimited download size and more fields (but is slower).
+fabaceae <- occurrences(taxon = "family:Fabaceae", qa = "none",
+                 download_reason_id = "testing")
 
-## You should adjust the `download_reason_id` to match your purposes if using
-##   this function for your own analyses; see `ala_reasons()`
 
-x <- occurrences(taxon = "family:Fabaceae", wkt = wkt, qa = "none",
-                 download_reason_id = "testing", extra = env_layers)
 
-xgridded <- x$data %>%
+fabaceaedat <- fabaceae$data %>%
   ## discard genus- and higher-level records
   dplyr::filter(rank %in%
                   c("species", "subspecies", "variety", "form", "cultivar")) %>%
   
   ## bin into 0.5-degree bins
-  mutate(longitude=round(longitude*2)/2, latitude=round(latitude*2)/2) %>%
+  mutate(longitude = round(longitude*2)/2, latitude = round(latitude*2)/2) %>%
+  group_by(longitude,latitude)  %>%
+  dplyr::select(year, month, eventDate, longitude, latitude, scientificNameOriginal, 
+                state) 
   
-  ## average environmental vars within each bin
-  group_by(longitude,latitude) %>%
-  mutate(precipitationAnnual=mean(precipitationAnnual, na.rm=TRUE),
-         temperatureAnnualMaxMean=mean(temperatureAnnualMaxMean, na.rm=TRUE)) %>%
-  
-  ## subset to vars of interest
-  dplyr::select(longitude, latitude, scientificName, precipitationAnnual,
-                temperatureAnnualMaxMean) %>%
-  
-  ## take one row per cell per species (presence)
-  distinct() %>%
-  
-  ## calculate species richness
-  mutate(richness=n()) %>%
-  
-  ## convert to wide format (sites by species)
-  mutate(present=1) %>%
-  do(tidyr::spread(data=., key=scientificName, value=present, fill=0)) %>%
-  ungroup()
+#remove NA
+#How is the temporal evolution of fabaceae?
+#select data from 1900
+fabaceaedat %>% group_by( year) %>% summarise(total=n()) %>% drop_na() %>%
+  filter(year>1900) %>%
+  ggplot(aes(y=total, x=year)) + geom_point() + geom_line() 
 
-## where a species was not present, it will have NA: convert these to 0
-sppcols <- setdiff(names(xgridded),
-                   c("longitude", "latitude", "precipitationAnnual", "temperatureAnnualMaxMean",
-                     "richness"))
-xgridded <- xgridded %>% mutate_at(sppcols, function(z) ifelse(is.na(z), 0, z))
+#Is this evolution different by state?
+fabaceaedat %>% group_by( year, state) %>% summarise(total=n()) %>% drop_na() %>%
+filter(year>1900) %>%
+  ggplot(aes(y=total, x=year, color=state)) + geom_point() + geom_line() 
+ 
+# can not select more than one taxon at the same time
+triodia <- occurrences(taxon = "triodia", qa = "none",
+                        download_reason_id = "testing")$data %>% mutate(plant ="Triodia")
 
-ggplot(xgridded, aes(longitude, richness)) + geom_point() + theme_bw()
 
+brachychiton <- occurrences(taxon = "brachychiton", qa = "none",
+                       download_reason_id = "testing")$data %>% mutate(plant ="Brachychiton")
+
+
+flindersia <- occurrences(taxon = "flindersia", qa = "none",
+                            download_reason_id = "testing")$data %>% mutate(plant ="Flindersia")
+
+
+livistona <- occurrences(taxon = "livistona", qa = "none",
+                          download_reason_id = "testing")$data %>% mutate(plant ="Livistona")
+
+
+callitris <- occurrences(taxon = "callitris", qa = "none",
+                         download_reason_id = "testing")$data %>% mutate(plant ="Callitris")
+
+
+daviesia <- occurrences(taxon = "daviesia", qa = "none",
+                         download_reason_id = "testing")$data %>% mutate(plant ="Daviesia")
+
+ficus <- occurrences(taxon = "ficus", qa = "none",
+                        download_reason_id = "testing")$data %>% mutate(plant ="Ficus")
+
+
+hakea <- occurrences(taxon = "hakea", qa = "none",
+                     download_reason_id = "testing")$data %>% mutate(plant ="Hakea")
+
+
+
+datos <- rbind(triodia, brachychiton, flindersia, livistona,
+               callitris, daviesia, ficus, hakea) %>%  dplyr::filter(rank %in%
+                c("species", "subspecies", "variety", "form", "cultivar")) %>%
+
+  mutate(longitude = round(longitude*2)/2, latitude = round(latitude*2)/2) %>%
+  group_by(longitude,latitude)  %>%
+  dplyr::select(year, month, eventDate, longitude, latitude, scientificNameOriginal, 
+                state, plant) 
+
+datos %>% drop_na() %>% filter(state!="") %>%group_by( year,state) %>% summarise(total=n()) %>%
+  filter(year>1990) %>%
+  ggplot(aes(y=total, x=year)) + geom_point() + geom_line() + facet_wrap(~state) 
+
+datos %>% drop_na() %>% filter(state!="") %>%group_by( year, plant) %>% summarise(total=n()) %>%
+  filter(year>1990) %>%
+  ggplot(aes(y=total, x=year, color=plant)) + geom_point() + geom_line() + 
+  scale_color_brewer(palette = "Dark2") 
+
+#Is this evolution different by state?
+datos %>% drop_na() %>%filter(state!="")%>%group_by( year,  plant, state) %>% summarise(total=n()) %>%
+  filter(year>1990) %>%
+  ggplot(aes(y=total, x=year, color=plant)) + geom_point() + geom_line() + facet_wrap(~state) +
+  scale_colour_brewer(palette = "Dark2") + theme(legend.position = "bottom")
+#
